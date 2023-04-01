9829cfec36ec3a7b717f9e576dc65e376dc1bd22
andreas.bendig <andreas.bendig@bmsoft.de>
2023-04-01 15:40:41 +0200

    Anpassung zur Verwendung des Odrners für Verzeichnisse. Werden Verzeichnisse gefunden, werden diese in die Abarbeitungsliste aufgenommen und im Nachgang abgearbeitet.

--- model/DirectoryScanner.py
+++ model/DirectoryScanner.py
@@ -21,31 +21,45 @@ class DirectoryScanner:
     def startScan(self):
         print("starte Scan...")
 
-        with os.scandir(self._Config.str_root_folder) as entries:
-            for entry in entries:
-                if entry.is_dir():
-                    my_dir: DirectoryEntry = DirectoryEntry.DirectoryEntry()
+        int_file_counter: int = 0
+        int_dir_counter:int =0
 
-                    my_dir.readEntry(entry)
+        if len(self._FolderList) > 0:
+            str_current_folder = next(iter(self._FolderList), None)
 
-                    print("Verzeichnis: " + my_dir.str_path)
-                    print(" --> Erstellt am: " + str(datetime.fromtimestamp(my_dir.flt_created_date)))  # TODO: Nur für Windows-Systeme
-                    print(" --> Letze Bearbeitung: " + str(datetime.fromtimestamp(my_dir.flt_modified_date)))
-                    print(" --> User ID: " + str(my_dir.int_group_id))
-                    print(" --> Group ID: " + str(my_dir.int_group_id))
+            while str_current_folder is not None:
+                self._FolderList.remove(str_current_folder)
+                with os.scandir(str_current_folder) as entries:
+                    for entry in entries:
+                        if entry.is_dir():
+                            my_dir: DirectoryEntry = DirectoryEntry.DirectoryEntry()
+                            my_dir.readEntry(entry)
 
-                else:
-                    my_file: Entry = Entry.Entry()
+                            self._FolderList.append(my_dir.str_path)
 
-                    my_file.readEntry(entry)
+                            int_dir_counter +=1
+                            print("Verzeichnis: " + my_dir.str_path)
+                            print(" --> Erstellt am: " + str(datetime.fromtimestamp(my_dir.flt_created_date)))  # TODO: Nur für Windows-Systeme
+                            print(" --> Letze Bearbeitung: " + str(datetime.fromtimestamp(my_dir.flt_modified_date)))
+                            print(" --> User ID: " + str(my_dir.int_group_id))
+                            print(" --> Group ID: " + str(my_dir.int_group_id))
 
-                    print("Datei: " + my_file.str_name)
-                    print(" --> Dateigröße: " + str(my_file.int_size) + " Bytes")
-                    print(" --> Erstellt am: " + str(datetime.fromtimestamp(my_file.flt_created_date)))  # TODO: Nur für Windows-Systeme
-                    print(" --> Letze Bearbeitung: " + str(datetime.fromtimestamp(my_file.flt_modified_date)))
-                    print(" --> User ID: " + str(my_file.int_user_id))
-                    print(" --> Group ID: " + str(my_file.int_group_id))
-                    print(" --> MD5- Hash: " + str(my_file.str_hash_value_md5))
-                    print(" --> Dateityp: " + str(my_file.str_extension))
+                        else:
+                            my_file: Entry = Entry.Entry()
+
+                            my_file.readEntry(entry)
+                            int_file_counter +=1
+                            print("Datei: " + my_file.str_name)
+                            print(" --> Dateigröße: " + str(my_file.int_size) + " Bytes")
+                            print(" --> Erstellt am: " + str(datetime.fromtimestamp(my_file.flt_created_date)))  # TODO: Nur für Windows-Systeme
+                            print(" --> Letze Bearbeitung: " + str(datetime.fromtimestamp(my_file.flt_modified_date)))
+                            print(" --> User ID: " + str(my_file.int_user_id))
+                            print(" --> Group ID: " + str(my_file.int_group_id))
+                            print(" --> MD5- Hash: " + str(my_file.str_hash_value_md5))
+                            print(" --> Dateityp: " + str(my_file.str_extension))
+
+                str_current_folder = next(iter(self._FolderList), None)
 
 
+        print("Anzahl der gescannten Verzeichnisse: " + str(int_dir_counter))
+        print("Anzahl der gescannten Dateien: " + str(int_file_counter))
