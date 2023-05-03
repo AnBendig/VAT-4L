f09720714c70e99d0de07f056dc44c8202fed6da
Andreas Bendig <andreas_bendig@andreasbendig.de>
2023-05-03 16:38:28 +0200

    Anpassung der API-Aufrufe: - Hinzufügen von Vergleich und Unterschieds-Abfragen mit SQL - Anpassung des Frontends zur Darstellung von Unterschieden in ausgewählten Jobs

--- api.py
+++ api.py
@@ -52,7 +52,6 @@ async def get_api_info():
 async def get_job_list():
     str_query = "SELECT * FROM tbl_job"
     result = sql_connector.readData(str_query)
-    # return dumps(result, default=str)
     return result
 
 @application.get("/vat4l-api/getjob/{job_id}")
@@ -62,18 +61,25 @@ async def get_job_by_id(job_id : int):
 
     result = sql_connector.readData(str_query)
     return result
-"""
-    if job_id.__le__(len(result)) and job_id.__gt__(0):
-        # return dumps(result[job_id - 1], default=str)
-        return result
-    else:
-        return "Index out of bounds!"
-"""
+
 
 @application.get("/vat4l-api/dirs/{job_id}")
 async def get_directories(job_id: int):
     str_query = "SELECT * FROM `tbl_scan` WHERE is_directory=TRUE AND job_id=" + str(job_id) + " ORDER BY path"
     result = sql_connector.readData(str_query)
 
-    # return dumps(result, default=str)
+    return result
+
+@application.get("/vat4l-api/comparejobs/{source_id}/{target_id}")
+async def get_compare_jobs(source_id : int, target_id : int):
+    str_query = "SELECT s.id as source_id, t.id as target_id, s.path as path, s.job_id as source_job_id, t.job_id as target_job_id, s.size as source_size, t.size as target_size, s.hash as source_hash, t.hash as target_hash, s.user_name as source_user_name, t.user_name as target_user_name, s.group_name as source_group_name, t.group_name as target_group_name, s.filemode as source_filemode, t.filemode as target_filemode, s.created_date as source_created_date, t.created_date as target_created_date, s.modified_date as source_modified_date, t.modified_date as target_modified_date FROM tbl_scan AS s JOIN tbl_scan AS t ON s.path=t.path WHERE s.job_id="+str(source_id)+" AND t.job_id="+str(target_id)+" AND (s.hash!=t.hash OR s.user_id!=t.user_id OR s.group_id!=t.group_id OR s.filemode!=t.filemode)"
+    result = sql_connector.readData(str_query)
+
+    return result
+
+@application.get("/vat4l-api/getdelta/{source_id}/{target_id}")
+async def get_delta_jobs(source_id: int, target_id : int):
+    str_query = "SELECT * FROM ( SELECT * FROM tbl_scan s WHERE s.job_id="+ str(source_id) +" UNION ALL SELECT * FROM tbl_scan t WHERE t.job_id="+ str(target_id) +") tbl_scan GROUP BY path HAVING COUNT(*) = 1"
+    result = sql_connector.readData(str_query)
+
     return result
--- view/app.js
+++ view/app.js
@@ -1,5 +1,6 @@
 let container = "";
 let tbl_header = "";
+let api_request = "http://practice.test7.bmsoft.de:8000/vat4l-api";
 
 function vat4lApp() {
 
@@ -7,23 +8,80 @@ function vat4lApp() {
         container = document.querySelector("#container")
         this.getJobList();
     }
+
     this.getJobList = function () {
-        fetch('http://practice.test7.bmsoft.de:8000/vat4l-api/jobs')
+        fetch(api_request + '/jobs')
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
-                this.tableHeader()
-               job_column_names=["Auswahl","ID","Start","Ende","verarbeite Verzeichnisse","verarbeitete Dateien"];
-               job_column_names.forEach(this.printHeader);
-
-               for (let i=0;i<data.length;i++){
-                    this.printJobs(data[i]);
-                }
+                let job_column_names=["Auswahl","Start","Ende","verarbeite Verzeichnisse","verarbeitete Dateien"];
+                this.printJobList(job_column_names, data)
             })
             .catch((error) => {
                 console.error(error);
             })
+    }
+
+    this.getResultList = function(source,target) {
+        fetch(api_request + '/getdelta/' + source + '/' + target)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                let column_names=["ID","Job ID","Pfad","Größe","Hash-Wert","Benutzer","Gruppe","Dateirechte","erstellt am", "letzte Änderung am"];
+                this.printResultList(column_names, data)
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
+    this.printJobList = function (cols, data) {
+         this.tableHeader()
+               cols.forEach(this.printHeader);
+                let html=``;
+
+               for (let i=0;i<data.length;i++){
+                   let job= data[i];
+                     html =`
+                         <div class="row" id="job-element-${job.id_job}">${
+                            this.printCol("checkbox", "text-start", job.id_job) +
+                            this.printCol("date", "text-end", job.dt_start_job) +
+                            this.printCol("date", "text-end", job.dt_end_job) +
+                            this.printCol("text", "text-center", job.int_processed_dir) +
+                            this.printCol("text", "text-center", job.int_processed_file)}
+                         </div>
+                        `;
+                    container.insertAdjacentHTML('beforeend', html)
+                }
+    }
+
+     this.printResultList = function (cols, data) {
+         this.tableHeader()
+               cols.forEach(this.printHeader);
+                let html=``;
+
+               for (let i=0;i<data.length;i++){
+                   let element= data[i];
+
+                     html =`
+                         <div class="row" id="job-element-${element.id_job}">${
+                            this.printCol("text", "text-start", element.id) +
+                            this.printCol("checkbox", "text-start", element.job_id) +
+                            this.printCol("text", "text-start", element.path) +
+                            this.printCol("text", "text-end", element.size) +
+                            this.printCol("text", "text-end", element.hash) +
+                            this.printCol("text", "text-end", element.user_name) + 
+                            this.printCol("text", "text-center", element.group_name)+ 
+                            this.printCol("text", "text-center", element.filemode)+ 
+                            this.printCol("date", "text-end", element.created_date)+ 
+                            this.printCol("date", "text-end", element.modified_date)
+                   }
+                         </div>
+                        `;
+                    container.insertAdjacentHTML('beforeend', html)
+                }
     }
 
     this.printHeader= function(column_name){
@@ -45,50 +103,71 @@ function vat4lApp() {
         tbl_header=document.querySelector("#tbl-header");
     }
 
-    this.printJobs = function (job) {
-        let html = `
-            <div class="row" id="job-element-${job.id_job}">
-                <div class="col">
-                    <div class="input-group mb-3">
-                      <div class="input-group-text">
-                        <input class="form-check-input mt-0" type="checkbox" value="${job.id_job}">
-                      </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="text-center">
-                        ${job.id_job}
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="text-end">
-                        ${job.dt_start_job.replace("T"," - ")}
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="text-end">
-                        ${job.dt_end_job.replace("T"," - ")}
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="text-center">
-                        ${job.int_processed_dir}
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="text-center">
-                        ${job.int_processed_file}
-                    </div>
-                </div>
-            </div>
-            
-        `;
+    this.printCol = function(type,align,field) {
+       let html=``;
 
-        container.insertAdjacentHTML('beforeend', html)
+        switch (type) {
+            case "checkbox":
+               html= `<div class="col">
+                        <div class="input-group mb-3">
+                          <div class="input-group-text">
+                            <input class="form-check-input mt-0" type="checkbox" id="selectID${field}" name="selectID" ="${field}" value="${field}">
+                            <label for="selectID${field}">&nbsp;${field}</label> 
+                          </div>
+                       </div>
+                      </div>`
+                break;
+            case "text":
+                html=` <div class="col">
+                    <div class="${align}">
+                         ${field}
+                    </div>
+                </div>`
+                break;
+
+            case "date":
+                html= `<div class="col">
+                    <div class="${align}">
+                        ${field.replace("T"," - ")}
+                    </div>
+                </div>`
+        }
+
+        return html;
     }
+
+
+
+}
+function getSelectedJobs(){
+    let selectedJobs= [-1,-1];
+
+
+    let element= document.getElementsByName("selectID");
+    for (var i=0; i<= element.length;i++ ){
+        if(element[i].checked){
+            if (selectedJobs[0] === -1) selectedJobs[0]=element[i].value;
+            else {
+                selectedJobs[1]=element[i].value;
+                return (selectedJobs);
+            }
+        }
+    }
+    return null;
 }
 
+function showSelectedJobs() {
+    selection = getSelectedJobs();
+    alert("Die folgenden Jobs wurden ausgewählt: " + getSelectedJobs().join(", "))
+    clearContent();
+    app.getResultList(selection[0],selection[1])
+}
+
+function clearContent() {
+        document.querySelector("#container").innerHTML="";
+    }
+
 document.addEventListener("DOMContentLoaded", () => {
-    let app = new vat4lApp();
+    app = new vat4lApp();
     app.init();
 });
