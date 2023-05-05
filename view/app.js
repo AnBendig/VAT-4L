377a629078cc40964803ba9e433feb21ef8fafc3
Andreas Bendig <andreas_bendig@andreasbendig.de>
2023-05-05 09:29:37 +0200

    Anpassung der Ergebnisliste für den Vergleich zweier Jobs

--- view/app.js
+++ view/app.js
@@ -15,120 +15,191 @@ function vat4lApp() {
                 return response.json();
             })
             .then((data) => {
-                let job_column_names=["Auswahl","Start","Ende","verarbeite Verzeichnisse","verarbeitete Dateien"];
-                this.printJobList(job_column_names, data)
+                let col_size = ["2", "3", "3", "2", "2"];
+                let job_column_names = ["Auswahl", "Start", "Ende", "verarbeite Verzeichnisse", "verarbeitete Dateien"];
+
+                container.insertAdjacentHTML('beforeend', this.printJobList(job_column_names, col_size, data));
             })
             .catch((error) => {
                 console.error(error);
             })
     }
 
-    this.getResultList = function(source,target) {
+
+    this.getResultList = function (source, target) {
+
+        let col_size = ["1", "1", "2", "1", "1", "1", "1", "1", "1", "2"];
+        let column_names = ["ID", "Job ID", "Pfad", "Größe", "Hash-Wert", "Benutzer", "Gruppe", "Dateirechte", "erstellt am", "letzte Änderung am"];
+
         fetch(api_request + '/getdelta/' + source + '/' + target)
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
-                let column_names=["ID","Job ID","Pfad","Größe","Hash-Wert","Benutzer","Gruppe","Dateirechte","erstellt am", "letzte Änderung am"];
-                this.printResultList(column_names, data)
+
+                let deleted_data = [];
+                let created_data = [];
+
+                for (let i = 0; i <= data.length - 1; i++) {
+                    if (data[i].job_id.toString() === source) {
+                        deleted_data.push(data[i])
+                    } else {
+                        created_data.push(data[i])
+                    }
+                }
+
+                if (created_data.length > 0) {
+                    this.addHeadLine("createdData", "Neue Elemente:", "#container");
+                    let html1 = this.printResultList(column_names, col_size, "tbl_created", "#createdData", created_data);
+                    document.querySelector("#createdData").insertAdjacentHTML('beforeend', html1);
+                }
+                if (deleted_data.length > 0) {
+                    this.addHeadLine("deletedData", "Gelöschte Elemente:", "#container");
+                    let html2 = this.printResultList(column_names, col_size, "tbl_deleted", "#deletedData", deleted_data);
+                    document.querySelector("#deletedData").insertAdjacentHTML('beforeend', html2);
+                }
             })
             .catch((error) => {
                 console.error(error);
+            }
+        )
+
+         fetch(api_request + '/comparejobs/' + source + '/' + target)
+            .then((response) => {
+                return response.json();
             })
-    }
+            .then((data) => {
+                let changed_data = [];
 
-    this.printJobList = function (cols, data) {
-         this.tableHeader()
-               cols.forEach(this.printHeader);
-                let html=``;
-
-               for (let i=0;i<data.length;i++){
-                   let job= data[i];
-                     html =`
-                         <div class="row" id="job-element-${job.id_job}">${
-                            this.printCol("checkbox", "text-start", job.id_job) +
-                            this.printCol("date", "text-end", job.dt_start_job) +
-                            this.printCol("date", "text-end", job.dt_end_job) +
-                            this.printCol("text", "text-center", job.int_processed_dir) +
-                            this.printCol("text", "text-center", job.int_processed_file)}
-                         </div>
-                        `;
-                    container.insertAdjacentHTML('beforeend', html)
+                for (let i = 0; i <= data.length - 1; i++) {
+                        changed_data.push(data[i])
                 }
+
+                if (changed_data.length > 0) {
+                    this.addHeadLine("changedData", "Geänderte Elemente:", "#container");
+                    let html = this.printResultList(column_names, col_size, "tbl_changed", "#changedData", changed_data);
+                    document.querySelector("#changedData").insertAdjacentHTML('beforeend', html);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            }
+        )
+
     }
 
-     this.printResultList = function (cols, data) {
-         this.tableHeader()
-               cols.forEach(this.printHeader);
-                let html=``;
+    this.printJobList = function (cols, col_size, data) {
+        this.tableHeader("job_header", "#container");
 
-               for (let i=0;i<data.length;i++){
-                   let element= data[i];
+        for (let i = 0; i <= cols.length - 1; i++) {
+            this.printHeader(cols[i], col_size[i], "#job_header")
+        }
 
-                     html =`
+        let html = ``;
+
+        for (let i = 0; i < data.length; i++) {
+            let job = data[i];
+            html += `
+                 <div class="row" id="job-element-${job.id_job}">${
+                this.printCol("checkbox", "text-center", job.id_job, col_size[0]) +
+                this.printCol("date", "text-center", job.dt_start_job, col_size[1]) +
+                this.printCol("date", "text-center", job.dt_end_job, col_size[2]) +
+                this.printCol("text", "text-center", job.int_processed_dir, col_size[3]) +
+                this.printCol("text", "text-center", job.int_processed_file, col_size[4])}
+                 </div>
+                `;
+            //container.insertAdjacentHTML('beforeend', html)
+        }
+
+        return html;
+    }
+
+    this.addHeadLine = function (id_headline, txt_headline, dom_element) {
+        let html = `
+            <div class="row"><h2>${txt_headline}</h2></div>
+            <div id="${id_headline}"></div> 
+        `;
+        document.querySelector(dom_element).insertAdjacentHTML('beforeend', html);
+    }
+
+    this.printResultList = function (cols, col_size, str_headerID, str_parentID, data) {
+        this.tableHeader(str_headerID, str_parentID)
+
+        for (let i = 0; i <= cols.length - 1; i++) {
+            this.printHeader(cols[i], col_size[i], "#" + str_headerID)
+        }
+
+        let html = ``;
+
+        for (let i = 0; i <= data.length - 1; i++) {
+            let element = data[i];
+
+            html += `
                          <div class="row" id="job-element-${element.id_job}">${
-                            this.printCol("text", "text-start", element.id) +
-                            this.printCol("checkbox", "text-start", element.job_id) +
-                            this.printCol("text", "text-start", element.path) +
-                            this.printCol("text", "text-end", element.size) +
-                            this.printCol("text", "text-end", element.hash) +
-                            this.printCol("text", "text-end", element.user_name) + 
-                            this.printCol("text", "text-center", element.group_name)+ 
-                            this.printCol("text", "text-center", element.filemode)+ 
-                            this.printCol("date", "text-end", element.created_date)+ 
-                            this.printCol("date", "text-end", element.modified_date)
-                   }
+                this.printCol("text", "text-center", element.id, col_size[0]) +
+                this.printCol("checkbox", "text-center", element.job_id, col_size[1]) +
+                this.printCol("text", "text-start", element.path, col_size[2]) +
+                this.printCol("text", "text-center", element.size, col_size[3]) +
+                this.printCol("text", "text-center", element.hash, col_size[4]) +
+                this.printCol("text", "text-center", element.user_name, col_size[5]) +
+                this.printCol("text", "text-center", element.group_name, col_size[6]) +
+                this.printCol("text", "text-center", element.filemode, col_size[7]) +
+                this.printCol("date", "text-center", element.created_date, col_size[8]) +
+                this.printCol("date", "text-center", element.modified_date, col_size[9])
+            }
                          </div>
                         `;
-                    container.insertAdjacentHTML('beforeend', html)
-                }
+        }
+        return html;
     }
 
-    this.printHeader= function(column_name){
-        html= `<div class="col">
+    this.printHeader = function (column_name, column_size, str_parent) {
+        let html = `<div class="col-sm-${column_size}">
                 <div class="text-center">
-                    ${column_name}
+                    <strong>${column_name}</strong>
                 </div>
             </div>
         `;
-        document.querySelector("#tbl-header").insertAdjacentHTML('beforeend', html)
+        document.querySelector(str_parent).insertAdjacentHTML('beforeend', html)
     }
 
-    this.tableHeader= function(){
-        html = `
-            <div class="row bg-body-secondary" id="tbl-header">            
+    this.tableHeader = function (str_headerID, str_parentID) {
+        let html = `
+            <div class="row bg-body-secondary" id="${str_headerID}">            
             </div>
         `;
-        container.insertAdjacentHTML('beforeend', html)
-        tbl_header=document.querySelector("#tbl-header");
+        document.querySelector(str_parentID).insertAdjacentHTML('beforeend', html)
+        //tbl_header = document.querySelector("#tbl-header");
     }
 
-    this.printCol = function(type,align,field) {
-       let html=``;
+    this.printCol = function (type, align, field, size) {
+        let html = ``;
 
         switch (type) {
             case "checkbox":
-               html= `<div class="col">
+                html = `<div class="col-sm-${size}">
                         <div class="input-group mb-3">
-                          <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" id="selectID${field}" name="selectID" ="${field}" value="${field}">
-                            <label for="selectID${field}">&nbsp;${field}</label> 
+                          <div class="input-group-text small">
+                            <span class="${align}">
+                                <input class="form-check-input mt-0" type="checkbox" id="selectID${field}" name="selectID" ="${field}" value="${field}">
+                                <label for="selectID${field}">&nbsp;${field}</label> 
+                            </span>
                           </div>
                        </div>
                       </div>`
                 break;
             case "text":
-                html=` <div class="col">
-                    <div class="${align}">
+                html = ` <div class="col-sm-${size}">
+                    <div class="${align} small">
                          ${field}
                     </div>
                 </div>`
                 break;
 
             case "date":
-                html= `<div class="col">
-                    <div class="${align}">
-                        ${field.replace("T"," - ")}
+                html = `<div class="col-sm-${size}">
+                    <div class="${align} small">
+                        ${field.replace("T", " - ")}
                     </div>
                 </div>`
         }
@@ -137,18 +208,18 @@ function vat4lApp() {
     }
 
 
-
 }
-function getSelectedJobs(){
-    let selectedJobs= [-1,-1];
+
+function getSelectedJobs() {
+    let selectedJobs = [-1, -1];
 
 
-    let element= document.getElementsByName("selectID");
-    for (var i=0; i<= element.length;i++ ){
-        if(element[i].checked){
-            if (selectedJobs[0] === -1) selectedJobs[0]=element[i].value;
+    let element = document.getElementsByName("selectID");
+    for (var i = 0; i <= element.length; i++) {
+        if (element[i].checked) {
+            if (selectedJobs[0] === -1) selectedJobs[0] = element[i].value;
             else {
-                selectedJobs[1]=element[i].value;
+                selectedJobs[1] = element[i].value;
                 return (selectedJobs);
             }
         }
@@ -158,14 +229,18 @@ function getSelectedJobs(){
 
 function showSelectedJobs() {
     selection = getSelectedJobs();
-    alert("Die folgenden Jobs wurden ausgewählt: " + getSelectedJobs().join(", "))
     clearContent();
-    app.getResultList(selection[0],selection[1])
+    app.getResultList(selection[0], selection[1])
+}
+
+function showJobList() {
+    clearContent();
+    app.getJobList();
 }
 
 function clearContent() {
-        document.querySelector("#container").innerHTML="";
-    }
+    document.querySelector("#container").innerHTML = "";
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     app = new vat4lApp();
